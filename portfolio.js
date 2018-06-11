3b8432775e19c01accf466cf8349c7f90dbbe157
bonnielt <32583035+bonnielt@users.noreply.github.com>
2018-06-11 10:25:07 -0700

    jun 7

--- portfolio.js
+++ portfolio.js
@@ -1,67 +1,78 @@
 (function() {
 
-    window.onload = function() {
-      $('body').removeClass('fade-out');
+  window.onload = function() {
+    $('body').removeClass('fade-out');
 
-        document.getElementsByClassName("navbar-toggle")[0].onclick = function() {
-            openNav();
-        };
 
-        document.getElementById("close").onclick = function() {
-            closeNav();
-        }
+    document.getElementsByClassName("navbar-toggle")[0].onclick = function() {
+      openNav();
+    };
 
-        $(document).ready(function() {
+    document.getElementById("close").onclick = function() {
+      closeNav();
+    };
 
-            /* Every time the window is scrolled ... */
-            $(window).scroll( function(){
+    document.getElementById("pizzaTrigger").onclick = function() {
+      document.body.style.backgroundColor="black";
+    }
 
-                /* Check the location of each desired element */
-                $('.hideme').each( function(i){
+    $(document).ready(function() {
 
-                    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-                    var bottom_of_window = $(window).scrollTop() + $(window).height();
+      /* Every time the window is scrolled ... */
+      $(window).scroll(function() {
 
-                    /* If the object is completely visible in the window, fade it it */
-                    if( bottom_of_window > bottom_of_object ){
+        /* Check the location of each desired element */
+        $('.hideme').each(function(i) {
 
-                        $(this).animate({'opacity':'1'},500);
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-                    }
+          /* If the object is completely visible in the window, fade it it */
+          if (bottom_of_window > bottom_of_object) {
 
-                });
+            $(this).animate({
+              'opacity': '1'
+            }, 500);
 
-            });
+          }
 
         });
 
-        document.getElementById("top-btn").onclick = function() {
-          topFunction();
-        }
+      });
 
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                document.getElementById("top-btn").style.display = "block";
-            } else {
-                document.getElementById("top-btn").style.display = "none";
-            }
-        }
+    });
 
-        // When the user clicks on the button, scroll to the top of the document
-        function topFunction() {
-            document.body.scrollTop = 0; // For Chrome, Safari and Opera
-            document.documentElement.scrollTop = 0; // For IE and Firefox
-        }
-    };
 
-    /* Open when someone clicks on the span element */
-    function openNav() {
-        document.getElementById("myNav").style.width = "100%";
+    document.getElementById("top-btn").onclick = function() {
+      topFunction();
     }
 
-    /* Close when someone clicks on the "x" symbol inside the overlay */
-    function closeNav() {
-        document.getElementById("myNav").style.width = "0%";
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("top-btn").style.display = "block";
+      } else {
+        document.getElementById("top-btn").style.display = "none";
+      }
     }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0; // For Chrome, Safari and Opera
+      document.documentElement.scrollTop = 0; // For IE and Firefox
+    }
+  };
+
+  /* Open when someone clicks on the span element */
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
 
 }());
